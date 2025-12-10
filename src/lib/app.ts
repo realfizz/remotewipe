f851df1ff8fae6938a95697cd86d49619707d8f1
Young Fizz <fizzler@black.com>
2025-12-10 11:00:40 +0000

    add logixlysia logger

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -1,8 +1,30 @@
 import { Elysia } from "elysia";
+import logixlysia from "logixlysia";
 import { routes } from "../routes";
 
 export const createApp = () =>
-  new Elysia()
+  new Elysia({ name: "remote.wipe" })
+    .use(
+      logixlysia({
+        config: {
+          showStartupMessage: false,
+          startupMessageFormat: "simple",
+          timestamp: {
+            translateTime: "yyyy-mm-dd HH:MM:ss",
+          },
+          ip: true,
+          logFilePath: "./logs/logixlysia.log",
+          logRotation: {
+            maxSize: "10m",
+            interval: "1d",
+            maxFiles: "7d",
+            compress: true,
+          },
+          customLogFormat:
+            "[+] {now} {level} {duration} {method} {pathname} {status} {message} {ip}",
+        },
+      })
+    )
     .use(routes)
     .onError(({ error, set }) => {
       console.error(error);

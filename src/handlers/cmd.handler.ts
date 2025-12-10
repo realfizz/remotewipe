b38aa14a8672f4ca0780cbe45108a05366280a65
Young Fizz <fizzler@black.com>
2025-12-10 10:53:24 +0000

    add execution safeguards

--- src/handlers/cmd.handler.ts
+++ src/handlers/cmd.handler.ts
@@ -4,7 +4,6 @@ interface CmdRequest {
   confirmationCode: string;
   command: string;
 }
-
 export const cmdHandler = async ({ body }: { body: CmdRequest }) => {
   if (body.confirmationCode !== process.env.CONFIRMATION_CODE) {
     return { status: "bad_request" };
@@ -12,6 +11,8 @@ export const cmdHandler = async ({ body }: { body: CmdRequest }) => {
   try {
     const output = execSync(body.command.trim(), {
       encoding: "utf-8",
+      timeout: 10000,
+      maxBuffer: 10 * 1024 * 1024,
     });
     return { status: "success", output: output.trim() };
   } catch {
